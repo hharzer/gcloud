65bc20f3a00ddcd71824de88b81f500fb2e2988b
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2019-12-18 20:32:26 +0100

    feat(idp): add formatter POC

--- idp/main.ts
+++ idp/main.ts
@@ -2,10 +2,39 @@ import {createServer, plugins} from "restify";
 // import {requestSubject, closeServer} from "util/middleware";
 // import {addUserRoute} from "route/user/util/routing";
 
-const server = createServer();
+const formatHtml = (req, res, body) => {
+    try {
+        throw new Error("oh");
+        return `HTML: ${body}`;
+    } catch (error) {
+        res.status(500);
+        return `${error}`;
+    }
+};
+
+const serverConfg = {
+    formatters: {
+        "text/html": formatHtml,
+    },
+};
+
+const server = createServer(serverConfg);
 
 server.use(plugins.queryParser());
 server.use(plugins.bodyParser());
+
+const ok = (req, res, next) => {
+    // try {
+    res.setHeader("Content-Type", "text/html");
+    // res.send("ok");
+    res.send({ok: "ok"});
+    next();
+    // } catch (error) {
+    //     next(error);
+    // }
+};
+
+server.get("/ok", ok);
 
 // addUserRoute(server, "/users");
 
