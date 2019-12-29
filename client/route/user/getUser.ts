6a6d5491f5e316e92f8307eabca13160af4d46cd
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2019-12-29 19:12:39 +0100

    feat(client): add OAuth 2.0 access token acquisition

--- client/route/user/getUser.ts
+++ client/route/user/getUser.ts
@@ -1,8 +1,53 @@
-import {getUser} from "route/user/util/database";
+import {getOauth2ClientCredentialsToken} from "util/oauth2";
+import {getUser} from "route/user/util/api";
+
+const MAX_RETRY = 2;
+
+const withOauth2Token = (clientId, clientSecret, scope, callApi) => {
+    let token: any = null;
+    return async (...args) => {
+        let retryCount = 0;
+        while (retryCount <= MAX_RETRY) {
+            console.log(`retryCount = ${retryCount}`);
+            try {
+                // check expiration
+                if (!token || retryCount > 0) {
+                    token = await getOauth2ClientCredentialsToken(
+                        clientId,
+                        clientSecret + "x",
+                        scope
+                    );
+                }
+                return await callApi(token.access_token, ...args);
+            } catch (error) {
+                // only 401 403
+                console.error("CATCH", error);
+                if (retryCount === MAX_RETRY) {
+                    console.error("THROW", error);
+                    throw error;
+                }
+            }
+            ++retryCount;
+        }
+    };
+};
+
+const getUserWithOauth2Token = withOauth2Token(
+    process.env.OAUTH2_CC_CLIENT_ID,
+    process.env.OAUTH2_CC_CLIENT_SECRET,
+    "custom1 custom2",
+    getUser
+);
 
 const executeRequest = async (req, res, next) => {
     try {
-        const users = await getUser();
+        // const token: any = await getOauth2ClientCredentialsToken(
+        //     process.env.OAUTH2_CC_CLIENT_ID,
+        //     process.env.OAUTH2_CC_CLIENT_SECRET,
+        //     "custom1 custom2"
+        // );
+        // const users = await getUser(token.access_token);
+        const users = await getUserWithOauth2Token();
         const response: any = {};
         response.title = "Users";
         response.homePath = "/";
--- client/route/user/util/api.ts
+++ client/route/user/util/api.ts
@@ -1,11 +1,14 @@
 import got from "got";
 
-export const getUser = async () => {
+export const getUser = async (accessToken) => {
     const apiHost = process.env.API_HOST;
     const apiPort = process.env.API_PORT;
     const userPath = "users";
     const options: any = {};
     options.prefixUrl = `http://${apiHost}:${apiPort}`;
+    const headers: any = {};
+    headers["Authorization"] = `Bearer ${accessToken}`;
+    options.headers = headers;
     const response: any = await got.get(userPath, options).json();
     const users = response.data;
     return users;
